5b1046829cdfafc32a89dfa237c5fc383ac8d0a9
nan.feilong <mkmonkey@qq.com>
2017-12-19 23:39:02 +0800

    定时任务相关代码

--- src/main/java/monkey/DemoApplication.java
+++ src/main/java/monkey/DemoApplication.java
@@ -2,8 +2,10 @@ package monkey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = {"classpath:spring-mvc.xml"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
