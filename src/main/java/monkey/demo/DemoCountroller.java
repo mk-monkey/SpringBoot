11f0c87f6874578b38465f6038d6d8c092e8b0e2
南飞龙 <nanfeilong@192.168.1.9>
2017-12-30 20:10:58 +0800

    swagger add

--- src/main/java/monkey/demo/DemoCountroller.java
+++ src/main/java/monkey/demo/DemoCountroller.java
@@ -1,17 +1,29 @@
 package monkey.demo;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/houzi")
+@RequestMapping(value = "/houzi")
+@EnableAutoConfiguration
 public class DemoCountroller {
     /**
      *
      */
-    @RequestMapping("/hello")
+    @RequestMapping("/mk")
+    //@ApiOperation(value = “接口说明”, httpMethod = “接口请求方式”, response = “接口返回参数类型”, notes = “接口发布说明”；其他参数可参考源码；
+    @ApiOperation(value = "mk",httpMethod ="GET", response = String.class,notes = "index")
     public String index() {
         return "我是一个小小小小小鸟";
+    }
+
+@RequestMapping("/monkey")
+//@ApiOperation(value = “接口说明”, httpMethod = “接口请求方式”, response = “接口返回参数类型”, notes = “接口发布说明”；其他参数可参考源码；
+@ApiOperation(value = "monkey",httpMethod ="GET", response = String.class,notes = "index")
+    public String index2() {
+        return "我是一个小小小小小鸟2";
     }
 
 
